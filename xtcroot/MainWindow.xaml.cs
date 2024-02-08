58c34b825a016c157d4435ab49d1ae4aa95a5b33
affggh <879632264@qq.com>
2024-02-08 15:39:02 +0800

    ui: Show warning before navigated to rootpage

--- xtcroot/MainWindow.xaml.cs
+++ xtcroot/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.UI.Composition.SystemBackdrops;
+﻿using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -8,6 +8,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -35,12 +36,42 @@ namespace xtcroot
             SelectedNavItem = navigationView.MenuItems[0];
         }
 
+        private async void OnNavigatingRootPage()
+        {
+            ContentDialog rootWarningDialog = new ContentDialog
+            {
+                Title = "⚠警告",
+                Content = "ROOT可能会导致您的设备损坏,如果因为您的操作不当造成设备损坏,Rexwe iMoo无任何责任\n" +
+              "如果是有人帮您付费代刷,恭喜您被骗了,请查看我们的官方教程https://b23.tv/94ugyjO\n" +
+              "在root之前,您必须掌握官方教程中的一些常用概念,否则您的设备可能会损坏,因此请确定您已经掌握了一些概念",
+                PrimaryButtonText = "我已知晓并继续",
+                CloseButtonText = "我不同意",
+                DefaultButton = ContentDialogButton.Primary,
+            };
+
+            rootWarningDialog.XamlRoot = this.Content.XamlRoot;
+            var result = await rootWarningDialog.ShowAsync();
+
+            if (result != ContentDialogResult.Primary)
+            {
+                contentFrame.Navigate(typeof(HomePage));
+                navigationView.SelectedItem = navigationView.MenuItems[0];
+            }
+
+        }
+
         private object selectedNavItem;
         public object SelectedNavItem
         {
             get { return selectedNavItem; }
-            set { selectedNavItem = value; }
+            set {
+                if (selectedNavItem != value)
+                {
+                    selectedNavItem = value;
+                }
+            }
         }
+
         private void NavigationView_Loaded(object sender, RoutedEventArgs e) // Set default frame
         {
             contentFrame.Navigate(typeof(HomePage));
@@ -63,12 +94,14 @@ namespace xtcroot
                         contentFrame.Navigate(typeof(RollBackPage));
                         break;
                     case "RootPage":
+                        OnNavigatingRootPage();
                         contentFrame.Navigate(typeof(RootPage));
                         break;
                     case "AboutPage":
                         contentFrame.Navigate(typeof(AboutPage));
                         break;
                 }
+                headerText.Text = invokedItem.Content.ToString();
             }
         }
     }
